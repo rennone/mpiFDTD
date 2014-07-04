18727130ebfc0029cc2e84413bfaba015a7c0023
Ren Mizutani <nightfall-immortal-longview@live.jp>
2014-07-04 17:04:07 +0900

    初期値をdefine文で設定するようにした.

--- multiLayerModel.c
+++ multiLayerModel.c
@@ -3,11 +3,33 @@
 #include "function.h"
 #include <math.h>
 
-double width_s[2];     //幅
-double thickness_s[2]; //厚さ
-double ep[2];           //誘電率 = n*n*ep0
-int layerNum;          //枚数
-bool notsymmetry;      //左右比対称
+//横幅
+#define ST_WIDTH_NM 300
+#define EN_WIDTH_NM 300
+#define DELTA_WIDTH_NM 10
+
+//ラメラの厚さ
+#define ST_THICK_NM 80
+#define EN_THICK_NM 80
+#define DELTA_THICK_NM 10
+
+//ラメラの枚数
+#define LAYER_NUM 8
+
+//互い違い
+#define ASYMMETRY false
+
+//屈折率
+#define N_1 1.0
+#define N_2 1.56
+
+static int width_nm[2] = {ST_WIDTH_NM, ST_WIDTH_NM};
+static int thickness_nm[2] = {ST_THICK_NM, ST_THICK_NM};
+static int layerNum = LAYER_NUM;     //枚数
+
+static double width_s[2];     //幅
+static double thickness_s[2]; //厚さ
+static double ep_s[2];        //誘電率 = n*n*ep0
 
 //col : D_Xモード row : D_Yモード
 //x,yを中心に, 計算領域のセルと同じ大きさの領域を調べる
@@ -21,7 +43,7 @@ static double eps(double x, double y, int col, int row)
 
   //領域の中心から, 下にheight/2ずれた位置がレイヤの下部
   int oy = fInfo_s.N_PY/2 - height/2;
-  int ox = fInfo_s.N_PX/2;  
+  int ox = fInfo_s.N_PX/2;
   double _x = x-ox;	//ox,oyを座標の原点に
   double _y = y-oy;
 
@@ -30,11 +52,12 @@ static double eps(double x, double y, int col, int row)
     return EPSILON_0_S;
 
   double s[2]={0,0}; //n1,n2それぞれの分割セルの数が入る
-
-  for(double i=-16+0.5; i<16; i+=1){
-    for(double j=-16+0.5; j<16; j+=1){
-      double sx = _x + col*i/32.0; //細分化したセルの位置
-      double sy = _y + row*j/32.0;
+  double split = 32;
+  double half_split = split/2;
+  for(double i=-half_split+0.5; i<half_split; i+=1){
+    for(double j=-half_split+0.5; j<half_split; j+=1){
+      double sx = _x + col*i/split; //細分化したセルの位置
+      double sy = _y + row*j/split;
 
       //上下に飛び出ていないか確認
       if(sy < 0 || sy > height)
@@ -52,36 +75,49 @@ static double eps(double x, double y, int col, int row)
 
       int k = (modY > thickness_s[0]); //どっちの屈折率にいるか調べる
 
-      if (sx < 0 && notsymmetry)
+      if (sx < 0 && ASYMMETRY)
         k = 1-k;		//左右で反転, 互い違いでなかったら反転しない
       
       if(abs(sx) < width_s[k]/2)
-        s[k] +=1;     
-
+        s[k] +=1;
     }    
   }
 
-  s[0] /= 32.0*32.0;
-  s[1] /= 32.0*32.0;
-  return EPSILON_0_S*(1-s[0]-s[1]) + ep[0]*s[0] + ep[1]*s[1];
+  s[0] /= split*split;
+  s[1] /= split*split;
+  return EPSILON_0_S*(1-s[0]-s[1]) + ep_s[0]*s[0] + ep_s[1]*s[1];
 }
 
 double ( *multiLayerModel_EPS(void))(double, double, int, int)
 {
-  width_s[0]     = field_toCellUnit(300);
-  width_s[1]     = field_toCellUnit(300);
-  thickness_s[0] = field_toCellUnit(90);
-  thickness_s[1] = field_toCellUnit(90);
-  layerNum = 8;
-  ep[0] = 1.56*1.56*EPSILON_0_S;
-  ep[1] = 1*1*EPSILON_0_S;
-
-  notsymmetry = true;
-
   return eps;
 }
 
 bool multiLayerModel_isFinish(void)
 {
   return true;
+}
+
+void multiLayerModel_needSize(int *x_nm, int *y_nm)
+{
+  (*x_nm) = max( width_nm[0], width_nm[1]);
+  (*y_nm) = (thickness_nm[0]+thickness_nm[1])*LAYER_NUM;
+}
+
+void multiLayerModel_moveDirectory()
+{
+  char buf[512];
+  sprintf(buf, "thick_%dnm_layer_%d", thickness_nm[0], layerNum);
+  makeDirectory(buf);
+  moveDirectory(buf);
+}
+
+void multiLayerModel_init()
+{
+  width_s[0]     = field_toCellUnit(width_nm[0]);
+  width_s[1]     = field_toCellUnit(width_nm[1]);
+  thickness_s[0] = field_toCellUnit(thickness_nm[0]);
+  thickness_s[1] = field_toCellUnit(thickness_nm[1]);
+  ep_s[0] = N_1*N_1*EPSILON_0_S;
+  ep_s[1] = N_2*N_2*EPSILON_0_S;
 }
